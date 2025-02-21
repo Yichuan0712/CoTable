76ee2ca6e2d024e7518185e7766c00eba02ac2b3
Yichuan <1758783038@qq.com>
2025-02-21 15:44:16 -0500

    Commit and Push

--- operations/f_split_table.py
+++ operations/f_split_table.py
@@ -1,17 +1,17 @@
 def f_split_table(row_groups, col_groups, df_table):
     subtables = {}
 
-    all_rows = set(range(df_table.shape[0]))
-    grouped_rows = set(sum(row_groups, []))
-    if all_rows != grouped_rows:
-        missing_rows = all_rows - grouped_rows
-        raise ValueError(f"Missing Rows: {missing_rows}")
-
-    all_cols = set(df_table.columns)
-    grouped_cols = set(sum(col_groups, []))
-    if all_cols != grouped_cols:
-        missing_cols = all_cols - grouped_cols
-        raise ValueError(f"Missing Cols: {missing_cols}")
+    # all_rows = set(range(df_table.shape[0]))
+    # grouped_rows = set(sum(row_groups, []))
+    # if all_rows != grouped_rows:
+    #     missing_rows = all_rows - grouped_rows
+    #     raise ValueError(f"Missing Rows: {missing_rows}")
+    #
+    # all_cols = set(df_table.columns)
+    # grouped_cols = set(sum(col_groups, []))
+    # if all_cols != grouped_cols:
+    #     missing_cols = all_cols - grouped_cols
+    #     raise ValueError(f"Missing Cols: {missing_cols}")
 
     for i, rows in enumerate(row_groups):
         for j, cols in enumerate(col_groups):
