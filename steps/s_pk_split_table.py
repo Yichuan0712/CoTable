f3264b37ff5d3175d4e431a2cbb0534cb2d44722
Yichuan <1758783038@qq.com>
2025-02-22 02:18:48 -0500

    Commit and Push

--- steps/s_pk_split_table.py
+++ steps/s_pk_split_table.py
@@ -81,7 +81,9 @@ def adjust_splits(row_groups, col_groups, df_table):
             _col_groups.remove(col_list)
 
     # remove duplicate
+    _col_groups = [tuple(x) for x in _col_groups]
     _col_groups = list(set(_col_groups))
+    _col_groups = [list(x) for x in _col_groups]
 
     return _row_groups, _col_groups
 
