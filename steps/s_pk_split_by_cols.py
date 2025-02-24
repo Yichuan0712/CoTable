db6fb45401f64791445ca655294b90d725190600
Yichuan <1758783038@qq.com>
2025-02-24 13:10:10 -0500

    Commit and Push

--- steps/s_pk_split_by_cols.py
+++ steps/s_pk_split_by_cols.py
@@ -42,7 +42,18 @@ def s_pk_split_by_cols_parse(content):
     return data
 
 
-def s_pk_split_by_cols(md_table, md_tables_split_by_rows, model_name="gemini_15_pro"):
+def s_pk_split_by_cols(md_table, model_name="gemini_15_pro", previous_col_groups=None):
+    if previous_col_groups:
+        print(0, "Based on the cached information.")
+        col_groups = [[fix_col_name(item, md_table) for item in group] for group in previous_col_groups]
+        df_table = f_split_by_cols(col_groups, markdown_to_dataframe(md_table))
+        return_md_tables = []
+        for d in df_table:
+            return_md_tables.append(dataframe_to_markdown(d))
+        for m in return_md_tables:
+            print(display_md_table(m))
+        return return_md_tables, True, "Based on the cached information.", 0, False, col_groups
+
     msg = s_pk_split_by_cols_prompt(md_table)
 
     messages = [msg, ]
@@ -54,16 +65,43 @@ def s_pk_split_by_cols(md_table, md_tables_split_by_rows, model_name="gemini_15_
 
     col_groups = s_pk_split_by_cols_parse(content)
     if col_groups is None:
-        return_md_tables = md_tables_split_by_rows
+        return_md_tables = [md_table, ]
     else:
         col_groups = [[fix_col_name(item, md_table) for item in group] for group in col_groups]
+        df_table = f_split_by_cols(col_groups, markdown_to_dataframe(md_table))
         return_md_tables = []
-        for mt in md_tables_split_by_rows:
-            df_table = f_split_by_cols(col_groups, markdown_to_dataframe(mt))
-            for d in df_table:
-                return_md_tables.append(dataframe_to_markdown(d))
+        for d in df_table:
+            return_md_tables.append(dataframe_to_markdown(d))
 
     for m in return_md_tables:
         print(display_md_table(m))
 
-    return return_md_tables, res, content, usage, truncated
+    return return_md_tables, res, content, usage, truncated, col_groups
+
+
+# split on the big table
+# def s_pk_split_by_cols(md_table, md_tables_split_by_rows, model_name="gemini_15_pro"):
+#     msg = s_pk_split_by_cols_prompt(md_table)
+#
+#     messages = [msg, ]
+#     question = ""
+#
+#     res, content, usage, truncated = get_llm_response(messages, question, model=model_name)
+#     # print(display_md_table(md_table))
+#     print(usage, content)
+#
+#     col_groups = s_pk_split_by_cols_parse(content)
+#     if col_groups is None:
+#         return_md_tables = md_tables_split_by_rows
+#     else:
+#         col_groups = [[fix_col_name(item, md_table) for item in group] for group in col_groups]
+#         return_md_tables = []
+#         for mt in md_tables_split_by_rows:
+#             df_table = f_split_by_cols(col_groups, markdown_to_dataframe(mt))
+#             for d in df_table:
+#                 return_md_tables.append(dataframe_to_markdown(d))
+#
+#     for m in return_md_tables:
+#         print(display_md_table(m))
+#
+#     return return_md_tables, res, content, usage, truncated
