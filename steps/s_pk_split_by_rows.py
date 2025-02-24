f69706863a6892eb946b56c7736a9202866c3921
Yichuan <1758783038@qq.com>
2025-02-23 21:21:57 -0500

    Commit and Push

--- steps/s_pk_split_by_rows.py
+++ steps/s_pk_split_by_rows.py
@@ -53,7 +53,9 @@ def s_pk_split_by_rows(md_table, model_name="gemini_15_pro"):
     if row_groups is None:
         return md_table, res, content, usage, truncated
     df_table = f_split_by_rows(row_groups, markdown_to_dataframe(md_table))
-    return_md_table = dataframe_to_markdown(df_table)
-    print(display_md_table(return_md_table))
+    return_md_tables = dataframe_to_markdown(df_table)
 
-    return return_md_table, res, content, usage, truncated
+    for m in return_md_tables:
+        print(display_md_table(m))
+
+    return return_md_tables, res, content, usage, truncated
