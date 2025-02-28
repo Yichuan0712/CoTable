dfaa92be1b43e4cf5c18f530ea0985817ab69ab8
Yichuan <1758783038@qq.com>
2025-02-28 12:00:43 -0500

    Commit and Push

--- steps/s_pk_get_parameter_type_and_unit.py
+++ steps/s_pk_get_parameter_type_and_unit.py
@@ -59,6 +59,9 @@ def s_pk_get_parameter_type_and_unit(col_dict, md_table, caption, model_name="ge
         print(usage, content)
 
         match_tuple = s_pk_get_parameter_type_and_unit_parse(content)
+
+        assert len(match_tuple[0]) == len(match_tuple[1]) == markdown_to_dataframe(md_table).shape[1]
+
         if match_tuple is None:
             raise NotImplementedError
         else:
