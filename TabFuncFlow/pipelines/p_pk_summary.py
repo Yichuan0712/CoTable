80b4e4c264d0be85d2d7747f78aca0e2e809ea71
Yichuan <1758783038@qq.com>
2025-03-09 00:58:55 -0500

    Commit and Push

--- TabFuncFlow/pipelines/p_pk_summary.py
+++ TabFuncFlow/pipelines/p_pk_summary.py
@@ -681,7 +681,7 @@ def p_pk_summary(md_table, description, llm="gemini_15_pro", max_retries=5, base
     Step 15: Post-Processing
     """
     """fix col name"""
-    expected_columns = ["Drug Name", "Analyte", "Specimen", "Population", "Pregnancy stage", "Subject N", "Parameter type", "Parameter unit", "Main value", "Statistics type", "Variation type", "Variation value", "Interval type", "Lower bound", "Upper bound", "P value"]
+    expected_columns = ["Drug name", "Analyte", "Specimen", "Population", "Pregnancy stage", "Subject N", "Parameter type", "Parameter unit", "Main value", "Statistics type", "Variation type", "Variation value", "Interval type", "Lower bound", "Upper bound", "P value"]
 
     def rename_columns(df, expected_columns):
         renamed_columns = {}
